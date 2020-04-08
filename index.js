1926f43138f03f64e867f71986140dbbaba61f65
kmw070806 <63104149+kmw070806@users.noreply.github.com>
2020-04-08 13:49:51 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+ 
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+// Set the prefix
+client.on("message", (message) => {
+
+  if (message.author.bot) return;
+
+  if (message.content.startsWith("안녕")) {
+    message.channel.send(`${`<@${message.author.id}> `}님 안녕하세요.`)
+  } 
+});
+ 
+client.login("NjkzNjM2OTEzOTQwMTM1OTc2.Xok1zg.4qTDLSi1ynvcLWbpmQPpxBpINEY");
